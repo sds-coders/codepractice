159cc42e4b33ecb524aa6227250d1125267d09e1
Woojong Kang <usoufo12@gmail.com>
2019-01-13 11:51:26 +0000

    report card: Add question PDF

---
+++ index.py
@@ -0,0 +1,12 @@
+print("Hello Python")
+
+def greetings(a):
+    print("This is %s" % a)
+    
+print(greetings("Woojong"))
+
+https://m.blog.naver.com/PostView.nhn?blogId=nb_m_awh1&logNo=220148658465&proxyReferer=https%3A%2F%2Fwww.google.com%2F
+    
+    https://www.nytimes.com/2018/08/28/business/energy-environment/california-clean-energy.html?rref=collection%2Fsectioncollection%2Fus&action=click&contentCollection=us&region=stream&module=stream_unit&version=latest&contentPlacement=2&pgtype=sectionfront
+        
+   통계     https://support.minitab.com/ko-kr/minitab/18/help-and-how-to/statistics/basic-statistics/supporting-topics/basics/example-of-getting-and-interpreting-a-p-value/
