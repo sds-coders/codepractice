d04006d084b70121983a1fb9c5677e29b386a862
Kim Jun-Sik <shjun4541@hotmail.com>
2019-01-16 14:16:36 +0000

    00

--- python/9_ex1_hydrangea.py
+++ python/9_ex1_hydrangea.py
@@ -1,6 +1,1 @@
-#준식이의 첫 github 사용입니다. 
-#다들 환호 안하고 뭐하나? ^^
-print("Hello World!")
-print("Hello again!")
-print("Why all this shit!")
-print("I am eager to hack into Dong-Un's Computer.")
+print "Hello World!"
