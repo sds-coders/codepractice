859a086e2a8276a532de77c5317510f358a8f0f5
Kim Jun-Sik <shjun4541@hotmail.com>
2019-01-18 14:53:04 +0000

    준식이의 일곱번째 예제

--- python/009_ex1_hydrangea.py
+++ python/009_ex1_hydrangea.py
@@ -1,1 +1,10 @@
+#-*- coding: utf-8 -*-
 print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun!"
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "...You touched it."
+#print "What's your problem?!"
