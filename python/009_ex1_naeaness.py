c81c7fce37ff74921b0d9ab5a2d28e5dfd06f737
Lee Yong Jin <cyberbrian@naver.com>
2019-01-20 11:56:19 +0000

    Move files in to the correct directory

--- python/009_ex1_naeaness.py
+++
@@ -1,9 +0,0 @@
-#-*-coding: utf-8 -*-
-print "Hello World!"
-print "Hello Again!"
-print "I like type this"
-print "This is fun"
-print "Yay! Printing"
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
-#print "이것은 더 해보기의 과제입니다."
