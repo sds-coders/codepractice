81d53e5fe79dc9f9857f46270e7e7429ce84caaf
Woojong <usoufo12@gmail.com>
2019-02-14 11:29:21 +0000

    42-43
    Signed-off-by: Woojong <usoufo12@gmail.com>

--- python/LearnPythonTheHardWay/163_oop_test_usoufo12.py
+++ python/LearnPythonTheHardWay/163_oop_test_usoufo12.py
@@ -28,28 +28,38 @@ PHRASES ={"class %%%(%%%):":
 			"*** 변수에서 *** 속성을 받아 *** 값으로 정한다."
 			}
 
-# 문장을 먼저 연습하고 싶은가
-if len(sys.argv) == 2 and sys.argv[1] == "한국어":
+# 문장을 먼저 연습하고 싶은지 확인
+if len(sys.argv) == 2 and sys.argv[1] == "한국어":  # sys.argv에 할당된 리스트의 길이 확인, 2번째 항목값 확인
 	PHRASE_FIRST = True
 else:
 	PHRASE_FIRST = False
 
-# 웹사이트에서 단어를 불러온다
-for word in urlopen(WORD_URL).readlines():
-	WORDS.append(word.strip()) # what does .strip() do? It probably remove the blank space at the both sides of the word.
-	
+
+for word in urlopen(WORD_URL).readlines():		  
+	WORDS.append(word.strip()) 
+# 사이트의 각 문장들을 항목으로 가지는 리스트의 각 항목들에 대하여 블록코드 실행.
+# 블록코드는 각 문장들을 스페이스 단위로 잘라서 나오는 단어들을 WORDS 리스트에 항목추가.
+# read() returns a string as a whole, while readlines() returns list that has strings corresponding to each line. It's why this code uses readlines() instead of read()
+# strip(char) return a copy of string which is deprived of char. It defaults blankspace(' ').
+# It seems better to use split() since the line could consist of more than one word.
+
 def convert(snippet, phrase):
 	class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
 	other_names = random.sample(WORDS, snippet.count("***"))
+	#snippet에 "%%%"가 몇개인지 확인하고, 그 값만큼  words에서 샘플을 뽑고 capitalize해서 리스트로 반환]
+	
 	results = []
 	param_names = []
 	
 	for i in range(0, snippet.count("@@@")):
-		param_count = random.randint(1,3)
+		param_count = random.randint(1, 3)
 		param_names.append(', '.join(random.sample(WORDS,param_count)))
-		
-	for sentence in snippet, phrase:
+	# snippet의 "@@@"개수 확인하고 블록코드 실행
+	# 블록코드는 WORDS에서 1개 또는 2개를 임의로 샘플링하고 ', ' 로 하나의 항목으로 연결시키고, param_names에 추가한다.
+	
+	for sentence in snippet, phrase: #??
 		result = sentence[:]
+		# sentence를 복사하여 추가
 		
 		# 가짜 클래스 이름
 		for word in class_names:
@@ -62,16 +72,21 @@ def convert(snippet, phrase):
 		# 가짜 매개변수 목록
 		for word in param_names:
 			result = result.replace("@@@", word, 1)
-			
+		# result의 "@@@"를 word로 변경.
+		# str.replace(old,new,count) : It substitute 'old' in the string with 'new', optionally limited by count.
 		results.append(result)
 		
 	return results
-
+	# %,@,*가 word in given url site로 변경된 result를 results에 저장.
+	
 # CTRL-D를 누를 때까지 계속한다
 try:
 	while True:
 		snippets = PHRASES.keys()
+		#Returns keys from PHRASE dict.
+		
 		random.shuffle(snippets)
+		#Return shuffled lst
 		
 		for snippet in snippets:
 			phrase = PHRASES[snippet]
@@ -81,8 +96,12 @@ try:
 				
 			print(question)
 			
-			input(">")
+			input(">"
 			print("답: %s\n\n" % answer)
-
-except E0FError:
+	#PHRASES에서 random한 순서로 key-value쌍을 호출, 내부의 @%*를 urlsite의 random word로 바꾸고 값 저장. Q-value, A-key, 정답출력
+	#snippet의 항목개수만큼 출력
+			
+except EOFError:
 	print("\nBye")
+				  
+	# EOFerror : End Of File Error,which occurs when built-in function like input() or raw_input() do not read any data before encountering the end of their input stream.
